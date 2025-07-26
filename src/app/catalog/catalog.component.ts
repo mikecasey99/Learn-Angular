c9043102c26a1796de24bc42cc934059e7a077e3
mike casey <mikecasey1120@gmail.com>
2025-07-25 23:30:01 -0400

    finished up to section 4.5

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IProduct } from './product.model';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+  product: IProduct;
+
+  constructor() {
+     this.product = 
+      {
+        id: 1,
+        description:
+          "A robot head with an unusually large eye and teloscpic neck -- excellent for exploring high spaces.",
+        name: "Large Cyclops",
+        imageName: "head-big-eye.png",
+        category: "Heads",
+        price: 1220.5,
+        discount: 0.2,
+      };
+  }
+
+   getImageUrl(product: IProduct) {
+    return '/assets/images/robot-parts/' + product.imageName;
+  }
+}
+
+
+
